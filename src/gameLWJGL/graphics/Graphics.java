a91741b9976ccf06a33a649e600eba2a22abd2a7
Juliane Fink <jule-fink@gmx.de>
2019-12-11 17:18:36 +0100

    add unused graphic thread class

---
+++ src/gameLWJGL/graphics/Graphics.java
@@ -0,0 +1,69 @@
+package gameLWJGL.graphics;
+
+import gameLWJGL.Window;
+import gameLWJGL.world.ObjectHandler;
+import gameLWJGL.world.World;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+
+public class Graphics implements Runnable {
+
+    private ObjectHandler objectHandler;
+    private World world;
+    private Window window;
+
+    private Thread thread;
+
+    private boolean shouldRender = false;
+    private boolean isRunning = false;
+
+    public Graphics(ObjectHandler objectHandler, World world) {
+        this.world = world;
+        this.objectHandler = objectHandler;
+
+        if(!glfwInit()){
+            throw new IllegalStateException("Failed to initialise GLFW.");
+        }
+        window = new Window(700, 700, "Game");
+        GL.createCapabilities();
+    }
+
+    public void start(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        while (isRunning){
+            if(!shouldRender){
+            world.render();
+            objectHandler.render();
+            window.swapBuffers();
+            shouldRender = false;
+            }
+        }
+        glfwTerminate();
+    }
+
+    public void stop(){
+        isRunning = false;
+        try{
+            thread.join();
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public void renderGame(){
+       shouldRender = true;
+    }
+
+    public Window getWindow(){
+        return window;
+    }
+}
