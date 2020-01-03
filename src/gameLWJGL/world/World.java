7c11c0c004e843a10bf892a11cef472ecd3f4d9e
Juliane Fink <jule-fink@gmx.de>
2020-01-03 14:23:21 +0100

    use function for world generation

--- src/gameLWJGL/world/World.java
+++ src/gameLWJGL/world/World.java
@@ -9,6 +9,7 @@ public class World {
 
     private List<GameObject> ground;
     private int numberOfGroundBoxes = 3;
+    private int worldSize = 10;
     private float groundBoxDistance = 0.3f;
 
     public World (){
@@ -16,10 +17,8 @@ public class World {
     }
 
     public void buildWorld(){
-        float distanceCounter = 0f;
-        for (int i = 0; i < numberOfGroundBoxes; i++){
-            GroundBlock block = new GroundBlock(-0.5f + distanceCounter,-0.5f, 0.2f, 0.02f);
-            distanceCounter += groundBoxDistance;
+        for (float x = 0; x < worldSize; x+=0.7f){
+            GroundBlock block = new GroundBlock(x, getY(x), 0.4f, 0.02f);
             ground.add(block);
         }
     }
@@ -30,6 +29,13 @@ public class World {
             tempObject.render();
         }
     }
+
+    private float getY(float x){
+        return (float)(0.4f * Math.cos(0.5f * x) - 0.4f * Math.cos(1f * x) - 0.4f * Math.cos(2f * x) + 0.3f);
+    }
+
+    // y= 0.4 cos(x) - 0.4 cos(2 x) - 0.4 cos(4 x) + 0.3
+
 
     public List<GameObject> getStaticObjects(){
         return ground;
