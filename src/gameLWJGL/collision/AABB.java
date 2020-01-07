09813d7bbe4065c28b5bb67d618ff092ce191976
Juliane Fink <jule-fink@gmx.de>
2020-01-07 20:52:00 +0100

    fix player to stand on ground

--- src/gameLWJGL/collision/AABB.java
+++ src/gameLWJGL/collision/AABB.java
@@ -7,6 +7,7 @@ public class AABB {
     public static Collision getCollision(GameObject a, GameObject b) // AABB - AABB collision
     {
         boolean isColliding = false;
+        CollisionDirection aMetBs = (a.y > b.y) ? CollisionDirection.UPSIDE : CollisionDirection.DOWNSIDE;
         if(Math.abs(a.x - b.x) < a.width + b.width)
         {
             if(Math.abs(a.y - b.y) < a.height + b.height)
@@ -14,7 +15,7 @@ public class AABB {
                 isColliding = true;
             }
         }
-        return new Collision( isColliding, new GameObject[]{a, b});
+        return new Collision( isColliding, new GameObject[]{a, b}, aMetBs);
     }
 
 }
--- src/gameLWJGL/physics/PhysicsObject.java
+++ src/gameLWJGL/physics/PhysicsObject.java
@@ -1,17 +1,23 @@
 package gameLWJGL.physics;
 
 import gameLWJGL.collision.Collision;
+import gameLWJGL.collision.CollisionDirection;
 import gameLWJGL.objects.GameObject;
 
 public abstract class PhysicsObject extends GameObject {
 
     public static final float FRICTION = 0.99f;
     public static final float GRAVITY = 0.002f;
+    public static final float GROUND_THRESHOLD = 0.0021f;
+    public static final float COLLISION_THRESHOLD = 0.00001f;
 
     protected float speedX = 0;
     protected float speedY = 0;
 
     protected float maxSpeed = 0.05f;
+
+    protected boolean isOnGround = false;
+    protected float isOnGroundAt = 0;
 
     protected PhysicsObject(float x, float y, float width, float height) {
         super(x, y, width, height);
@@ -32,13 +38,26 @@ public abstract class PhysicsObject extends GameObject {
         moveWithPhysics(speedX, speedY);
         speedX = Math.min(speedX * FRICTION, maxSpeed);
         speedY = Math.min(speedY * FRICTION, maxSpeed);
-        accelerate(0, -GRAVITY);
+        if(!isOnGround()){
+            accelerate(0, -GRAVITY);
+        }
+        isOnGround = false;
+    }
+
+    private boolean isOnGround(){
+        return isOnGround && Math.abs(y - isOnGroundAt) <  GROUND_THRESHOLD;
     }
 
 
     @Override
     public void handleCollision(Collision collisionData) {
-        //speedX = 0;
+        if(collisionData.aMetBs == CollisionDirection.UPSIDE) {
+            GameObject collidingGameObject = collisionData.gameObjects[1];
+            float upperBorder = collidingGameObject.y + collidingGameObject.height;
+            y = upperBorder + height - COLLISION_THRESHOLD;
+            isOnGroundAt = y;
+            isOnGround = true;
+        }
         speedY = 0;
     }
 }
